7ea7e9c8b96f4727199eadbec29a2a11258dc778
xiaomeng <282044472@qq.com>
2017-02-24 17:11:47 +0800

    项目初次构建

---
+++ README.md
@@ -0,0 +1,16 @@
+## 简趣
+
+###技术栈
+- React
+- React-Router
+- Redux
+- ES6
+- Less
+- Antd
+- AnimateCSS
+- Mock.js
+- antd
+
+
+2.24
+配置webpack与包管理
---
+++ webApp/src/main.js
@@ -0,0 +1,8 @@
+import React from 'react' ;
+import ReactDOM from 'react-dom' ;
+import './less/main' ;
+
+ReactDOM.render(
+  	<h1 className="redTitle">Helaaa <span>World</span></h1>,
+    document.getElementById('container')
+);
