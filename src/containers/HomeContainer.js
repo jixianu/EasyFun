9567b1c69c9fe2f6f9034fae8d6722e179c9dd62
xiaomeng <282044472@qq.com>
2017-03-06 22:03:03 +0800

    add fetchJSONP

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import fetchJsonp from 'fetch-jsonp'
 import Layout from 'antd/lib/layout'
 import 'antd/dist/antd.less'
 const { Content, Sider } = Layout
@@ -8,16 +8,28 @@ export default class HomeContainer extends Component {
   constructor(props){
     super(props);
     this.state = {};
+    this.handle = this.handle.bind(this);
   }
 
   componentDidMount() {
   }
-
+  handle() {
+    fetchJsonp('https://api.douban.com/v2/book/1220562')
+      .then(function(response) {
+        return response.json()
+      }).then(function(json) {
+        console.log('parsed json', json.id)
+      }).catch(function(ex) {
+        console.log('parsing failed', ex)
+      })
+  }
   render() {
     return (
       <Layout>
         <Sider>Sider</Sider>
-        <Content>Content</Content>
+        <Content>
+          <button onClick={()=>{this.handle()}}>aaa</button>
+        </Content>
       </Layout>
     )
   }
