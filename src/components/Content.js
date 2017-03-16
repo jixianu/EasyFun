6dd531cdfe170bcf481f2bc9a87fbd55b0b5e1d3
xiaomeng <282044472@qq.com>
2017-03-16 15:24:20 +0800

    分离header与footer

---
+++ src/components/Content.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react'
+
+export default class Content extends Component {
+  renderFilter(filter, name) {
+    if(filter == this.props.filter) {
+      return name;
+    }
+    return (
+      <a
+        href="#"
+        onClick={e => {
+          e.preventDefault();
+          this.props.onFilterChange(filter);
+        }}>
+        {name}
+      </a>
+    );
+  }
+
+  render() {
+    return (
+      <p>
+        SHOW
+        {' '}
+        {this.renderFilter('SHOW_ALL', 'All')}
+        {', '}
+        {this.renderFilter('SHOW_COMPLETED', 'Completed')}
+        {', '}
+        {this.renderFilter('SHOW_ACTIVE', 'Active')}
+        .
+      </p>
+    );
+  }
+}
